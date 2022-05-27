ec4210f391aea334c52a42cefcf67a134778ecee
sathish <89565652+sathish19961412@users.noreply.github.com>
2022-05-27 16:31:03 +0530

    Name Chages

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('PyCharm')
+    print('PyCharm1')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
